a1f331383dffca825b2a8e17960c2249ab55198e
Steve Kierstead <steve.kierstead@gmail.com>
2018-11-14 19:51:44 +0000

    switch from camelCase to underscores

--- gears.py
+++ gears.py
@@ -2,20 +2,20 @@
 
 class CGearCombo(object):
     def __init__(self, nFront, nRear):
-        self._nFront = nFront
-        self._nRear = nRear
+        self._num_front = nFront
+        self._num_rear = nRear
 
     def ratio(self):
         try:
-            rval = float(self._nFront) / self._nRear
+            rval = float(self._num_front) / self._num_rear
         except Exception as e:
             print("ERROR calculating ratio for f={}. r={}: {}".
-                    format(self._nFront, self._nRear, str(e)))
+                    format(self._num_front, self._num_rear, str(e)))
             rval = None
         return rval
 
-    def asString(self, verbose=False):
+    def as_string(self, verbose=False):
         fmt = "Front: {}, Rear: {}, Ratio {:.3f}" if verbose else "F:{} R:{} Ratio {:.3f}"
-        return fmt.format(self._nFront, self._nRear, self.ratio())
+        return fmt.format(self._num_front, self._num_rear, self.ratio())
 
 
--- testGears.py
+++ testGears.py
@@ -7,26 +7,26 @@ class CTestGears(unittest.TestCase):
     def setUp(self):
         pass
 
-    def utilTestRatio(self, front, rear):
+    def util_test_ratio(self, front, rear):
         ratio = float(front)/rear
         ratioAsStr = "{:.3f}".format(ratio)
         gc = gears.CGearCombo(front, rear)
         self.assertEqual(gc.ratio(), ratio)
-        self.assertEqual(gc.asString(True), "Front: {}, Rear: {}, Ratio {}".format(front, rear, ratioAsStr))
-        self.assertEqual(gc.asString(False), "F:{} R:{} Ratio {}".format(front, rear, ratioAsStr))
+        self.assertEqual(gc.as_string(True), "Front: {}, Rear: {}, Ratio {}".format(front, rear, ratioAsStr))
+        self.assertEqual(gc.as_string(False), "F:{} R:{} Ratio {}".format(front, rear, ratioAsStr))
 
-    def test_GearCombo(self):
-        # Simple first test case
+    def test_gear_combo(self):
+        # Simple first test case (with hard-coded results to be really, really sure)
         gc = gears.CGearCombo(23, 45)
         self.assertEqual(gc.ratio(), (float(23) / 45))
-        self.assertEqual(gc.asString(True), "Front: 23, Rear: 45, Ratio 0.511")
-        self.assertEqual(gc.asString(False), "F:23 R:45 Ratio 0.511")
+        self.assertEqual(gc.as_string(True), "Front: 23, Rear: 45, Ratio 0.511")
+        self.assertEqual(gc.as_string(False), "F:23 R:45 Ratio 0.511")
 
         # Another straightforward case
-        self.utilTestRatio(38, 19)
+        self.util_test_ratio(38, 19)
 
         # One more straightforward case
-        self.utilTestRatio(38, 16)
+        self.util_test_ratio(38, 16)
 
         # What if the denominator is zero?  We should get a None response and no crash.
         gc = gears.CGearCombo(38, 0)
